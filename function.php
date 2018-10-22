8b7806277597b93c66b53e16d428c33ab97defcb
martynasssz <martynasssk@gmail.com>
2018-10-22 21:09:04 +0300

    papildyti

---
+++ function.php
@@ -0,0 +1,8 @@
+<?php
+
+function temp() {
+	for (i=0; i<35;i++) {
+		echo "<td>$i</td> <br/>";
+	}
+}
+?>
---
+++ index.php
@@ -0,0 +1,83 @@
+<?php
+include ('function.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Silta_salta</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col"> 
+				<table class="table">
+		  <thead>
+		    <tr>
+		      <th scope="col">t °C</th>
+		      <th scope="col">Pojūtis</th>
+		  </thead>
+		  <tbody>
+		  	  <tr>
+		  	  	<?php
+				$feel= temp;
+				echo temp; 
+				?>
+			</tr>
+
+		    <tr>
+		      <th scope="row">1</th>
+		      <td>Mark</td>
+		      <td>Otto</td>
+		      <td>@mdo</td>
+		    </tr>
+		    <tr>
+		      <th scope="row">2</th>
+		      <td>Jacob</td>
+		      <td>Thornton</td>
+		      <td>@fat</td>
+		    </tr>
+		    <tr>
+		      <th scope="row">3</th>
+		      <td>Larry</td>
+		      <td>the Bird</td>
+		      <td>@twitter</td>
+		    </tr>
+		  </tbody>
+		</table>
+
+
+
+
+			</div>
+
+
+
+
+
+
+		</div
+
+
+
+
+
+
+
+
+
+	</di
+
+
+
+
+
+
+
+
+</body>
+</html>
